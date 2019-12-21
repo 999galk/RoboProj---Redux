ecdfacfc84c648ca0f83e4e7440731c5d75bf7e1
Gal Kol <999galk@gmail.com>
2019-12-21 13:08:51 +0200

    test of first component

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //here we import app.js, if we're importing js file we dont need to mention the .js at the end its the default
 import App from './App';
+//import FirstComponent from './FirstComponent';
 //service worker - new feature allows our apps to run faster and potentially run offline
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+
+//This is firstComponent example:
+//ReactDOM.render(<FirstComponent greeting={'Hello React Ninja!'}/>, document.getElementById('root'));
 
 //This is calling a class called 'app' to be rendered in the DOM element called root. To see the app go to app.js look for class app
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App greeting={'Hello React Ninja!'}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
