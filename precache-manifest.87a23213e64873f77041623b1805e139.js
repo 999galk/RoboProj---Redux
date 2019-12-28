3166774bf5d3cb08e8f163a9f11f75d6fd6dee45
Gal Kol <999galk@gmail.com>
2019-12-28 18:46:18 +0200

    Updates

---
+++ precache-manifest.87a23213e64873f77041623b1805e139.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a01e0d8e0e2eab484364c0483f253a",
+    "url": "/RoboProj--Redux/index.html"
+  },
+  {
+    "revision": "e02dcc6e2c87391ee8b9",
+    "url": "/RoboProj--Redux/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9478523c2b37ee6fe558",
+    "url": "/RoboProj--Redux/static/css/main.ea37a94c.chunk.css"
+  },
+  {
+    "revision": "e02dcc6e2c87391ee8b9",
+    "url": "/RoboProj--Redux/static/js/2.074a00ac.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/RoboProj--Redux/static/js/2.074a00ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9478523c2b37ee6fe558",
+    "url": "/RoboProj--Redux/static/js/main.ba7d6c5f.chunk.js"
+  },
+  {
+    "revision": "d7a6a1fdfccc07c0857f",
+    "url": "/RoboProj--Redux/static/js/runtime-main.ac333b5b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboProj--Redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
